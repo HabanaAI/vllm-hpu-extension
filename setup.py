1bf28929a6381f110bae6c1260cba4ab37c51edc
Konrad Zawora <kzawora@habana.ai>
2024-12-12 19:43:25 +0200

    Add version inference via setuptools-scm

--- setup.py
+++ setup.py
@@ -2,20 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="vllm-hpu-extension",
-    version="0.1",
     packages=find_packages(),
     install_requires=[],
-    author="Tomasz Zielinski",
-    author_email="tzielinski@habana.ai",
-    description="HPU extension package for vLLM",
     long_description="HPU extension package for vLLM. The package contains custom HPU-specific ops. It only works together with vLLM.",
     long_description_content_type="text/markdown",
-    url="https://github.com/tzielinski-habana/vllm-hpu-extension",
+    url="https://github.com/HabanaAI/vllm-hpu-extension",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache 2.0",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
 )
-
