44cc0bfeef6def92c72212b129c6c27fa9569c2f
Chendi Xue <chendi.xue@intel.com>
2024-12-19 22:02:10 +0200

    update const pipeline_pa and const_pa
    Signed-off-by: Chendi Xue <chendi.xue@intel.com>

--- vllm_hpu_extension/ops.py
+++ vllm_hpu_extension/ops.py
@@ -68,25 +68,29 @@ def group_sum(partial_sum, block_mapping, block_groups, type="reduce_sum_T"):
     return sums
 
 
+group_sum_tpc = os.environ.get('VLLM_SOFTMAX_TPC_GROUPSUM', 'false').lower() == 'true'
 def pipelined_const_pa(attn, value, block_groups, block_mapping, block_scales, batch_size,
                  matmul_av_op, batch2block_matmul_op, block2batch_matmul_op):
     # Normalize the attention scores
-    block_max = 10.0
-    attn = attn.sub(block_max)
+    block_max = torch.tensor(10.0, dtype=attn.dtype, device=attn.device)
+    attn.sub_(block_max)
     attn = attn.exp()
-    block_sums = attn.sum(dim=-1, keepdim=True)
-    adjustment_target_shape = block_sums.shape
-    attn = matmul_av_op(attn, value)
-    # start to calculate the block adjustment
-    block_sums = block_sums.squeeze()
-    # Sum block's sums that belongs to the same sequeneces => looks like this is a sync will happen after all slice done.
-    group_sums = group_sum(block_sums, block_mapping, block_groups, "reduce_sum_T")
+    # Sum block's sums that belongs to the same sequeneces
+    if not group_sum_tpc:
+        block_sums = attn.sum(dim=-1).unsqueeze(-1)
+        group_sums = group_sum(block_sums, block_mapping, block_groups, 'mme')
+    else:
+        block_sums = attn.sum(dim=-1, keepdim=True)
+        adjustment_target_shape = block_sums.shape
+        block_sums = block_sums.squeeze()
+        group_sums = group_sum(block_sums, block_mapping, block_groups, 'reduce_sum_T')
     # For stability in case some of the sums have been zeroed out during block aggretation
-    sums = torch.maximum(block_sums, group_sums)
-    # get a rescale factor which is 
-    rescale = torch.ones_like(sums).div(sums)
-    rescale = rescale.view(*adjustment_target_shape)
-    attn = attn.mul(rescale)
+    group_sums.add_(torch.finfo(group_sums.dtype).tiny)
+    group_sums = torch.maximum(block_sums, group_sums)
+    attn = matmul_av_op(attn, value)
+    if group_sum_tpc:
+        group_sums = group_sums.view(*adjustment_target_shape)
+    attn.div_(group_sums)
     return attn
 
 
@@ -138,8 +142,7 @@ def pa(attn, value, block_groups, block_mapping, block_scales, batch_size,
     sums = attn.sum(dim=-1).unsqueeze(-1)
     block_sum = sums
     # Sum block's sums that belongs to the same sequeneces
-    sums = block2batch(sums, block_mapping, block2batch_matmul_op)
-    group_sums = batch2block(sums, block_mapping, batch2block_matmul_op)
+    group_sums = group_sum(sums, block_mapping, block_groups, 'mme')
     group_sums.add_(torch.finfo(group_sums.dtype).tiny)
     group_sums = torch.maximum(block_sum, group_sums)
     attn.div_(group_sums)
