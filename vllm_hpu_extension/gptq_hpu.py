21d7388b7c33da3de1009751c0fc081ce5f9108a
maktukmak <mehmet.aktukmak@intel.com>
2025-01-09 22:54:39 +0000

    licence added, imports removed

--- vllm_hpu_extension/gptq_hpu.py
+++ vllm_hpu_extension/gptq_hpu.py
@@ -1,12 +1,23 @@
-import enum
-from enum import Enum
+# Copyright (c) 2025 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from fractions import Fraction
 from typing import Any, Dict, List, Optional
 
 import torch
 from torch.nn.parameter import Parameter
 
-from vllm import _custom_ops as ops
 from vllm.model_executor.layers.linear import LinearBase, LinearMethodBase
 from vllm.model_executor.layers.quantization.base_config import (
     QuantizationConfig)
