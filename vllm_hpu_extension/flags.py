b9514fcd2a795ea391fd89e73c8cf4a573690efe
Artur Fierka <afierka@habana.ai>
2025-01-21 12:55:17 +0200

    Fix condition to compile_one_hot flag

--- vllm_hpu_extension/flags.py
+++ vllm_hpu_extension/flags.py
@@ -147,7 +147,7 @@ def enabled_flags():
         "fsdpa": (Not(Hardware("cpu"))
                   & Kernel(fsdpa)
                   & EnvFlag("VLLM_PROMPT_USE_FUSEDSDPA", Not(ModelType('qwen2')))),
-        "compile_one_hot": VersionRange(">=20.0.0.370"),
+        "compile_one_hot": (VersionRange(">=1.20.0.370") & Not(EnvFlag("PT_HPU_LAZY_MODE", "1")))
     }
     environment = get_environment()
     detected = Flags(supported_flags, environment)
