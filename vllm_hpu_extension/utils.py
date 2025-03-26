a033df51c2017355fd1110c2fbab0e78a2e2dc6e
Karol Damaszke <kdamaszke@habana.ai>
2025-03-26 13:12:44 +0200

    Perform KV cache update on flat layout

--- vllm_hpu_extension/utils.py
+++ vllm_hpu_extension/utils.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (C) 2024 Habana Labs, Ltd. an Intel Company
+# Copyright (C) 2024-2025 Habana Labs, Ltd. an Intel Company
 #
 # This source code is licensed under the Apache 2.0 license found in the
 # LICENSE file in the root directory of this source tree.
@@ -59,8 +59,9 @@ class VLLMKVCache(torch.nn.Module):
         self.use_contiguous_pa = os.environ.get('VLLM_CONTIGUOUS_PA',
                                                 'true').lower() == 'true'
 
-    def forward(self, input, cache, block_indices, block_offset):
-        insert_or_update_cache(input, cache, block_indices, block_offset)
+    def forward(self, input, cache, block_indices, flat_indices_with_offsets):
+        insert_or_update_cache(input, cache, block_indices,
+                               flat_indices_with_offsets)
         return cache
 
     def fetch_from_cache(self, cache, blocks):
