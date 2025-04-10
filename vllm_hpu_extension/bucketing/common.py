539f356f4eb70a12dd62dc2e9ea3bd10aff1451b
Konrad Zawora <kzawora@habana.ai>
2025-04-10 20:14:46 +0300

    use weakrefs in singleton

--- vllm_hpu_extension/bucketing/common.py
+++ vllm_hpu_extension/bucketing/common.py
@@ -2,6 +2,7 @@
 import os
 from typing import Dict
 import inspect
+from weakref import WeakValueDictionary
 
 class Singleton(type):
     """
@@ -17,13 +18,16 @@ class Singleton(type):
             arguments used to create the instance are the same as the ones provided. Raises an assertion error if 
             the arguments differ.
     """
-    _instances: Dict[type, object] = {}
+    _instances: WeakValueDictionary[type, object] = WeakValueDictionary()
     _instances_argspec: Dict[type, object] = {}
 
     def __call__(cls, *args, **kwargs):
         argspec = inspect.getcallargs(super().__call__, args, kwargs)
         if cls not in cls._instances:
-            cls._instances[cls] = super().__call__(*args, **kwargs)
+            # NOTE(kzawora): *DO NOT* assign to self._instances[cls] here,
+            # we need this variable to to force a strong reference.
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
             cls._instances_argspec[cls] = argspec
         assert cls._instances_argspec[cls] == argspec, "Singleton instance already initialized with different arguments"
         return cls._instances[cls]
