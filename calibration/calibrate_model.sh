2b4ba562c43c538c2a502d19461f32cb43affbaa
Artur Fierka <artur.fierka@intel.com>
2025-04-18 12:47:15 +0200

    Calibration with multi-node (#145) (#157)
    * Add distributed backend parameter support to measure scales calibration step
    
    * Add distributed backend parameter support to quantize scales calibration step
    
    * Update calibration script to support multi-node setup
    
    * Update readme about multi-node calibration procedure
    
    * Replace string argument with choices
    
    * Use correct NOTE tag in readme
    
    * Typo
    
    * Limit max_model_len in step-4
    
    * Update groups support in step-5 unification script
    
    * Fix plenty of tiny bugs in calibration script
    
    * Update README
    
    * Polishing readme
    
    * Switch python to python3 in readme

--- calibration/calibrate_model.sh
+++ calibration/calibrate_model.sh
@@ -4,11 +4,12 @@
 ###############################################################################
 
 set -e
+cd "$(dirname "$0")"
 
 ALLOWED_DEVICES=("g2" "g3")
 
 usage() {
-    echo``
+    echo
     echo "Calibrate given MODEL_PATH for FP8 inference"
     echo
     echo "usage: ${0} <options>"
@@ -18,8 +19,19 @@ usage() {
     echo "  -o    - [required] path to output directory for fp8 measurements"
     echo "  -b    - batch size to run the measurements at (default: 32)"
     echo "  -l    - limit number of samples in calibration dataset"
-    echo "  -t    - tensor parallel size to run at (default: 1)"
+    echo "  -t    - tensor parallel size to run at (default: 1); NOTE: if t > 8 then we need a multi-node setup"
+    echo "  -g    - groups of cards we want to unify. Card indices seperated by commas and groups seperated by double dash '--', e.g. 0,1--2,3--4,5--6,7 card 0 measurement will be unified with card 1 measurement and so on."
     echo
+}
+
+cleanup_tmp() {
+	if [[ $(pwd) == *vllm-hpu-extension/calibration ]]; then
+		echo "Clearing temporary directory"
+		rm -rf nc_workspace
+		rm -rf inc_tmp
+	else
+		echo "Skipping temporary directory removal"
+	fi
 }
 
 create_measure_config() {
@@ -49,7 +61,7 @@ create_quant_config() {
     echo "$tmp_config" > $1/$2/maxabs_quant_$3.json
 }
 
-function extract_last_folder_name() {
+extract_last_folder_name() {
     local path="$1"
 
     path="${path%/}"
@@ -59,10 +71,13 @@ function extract_last_folder_name() {
     echo "$last_folder"
 }
 
+cleanup_tmp
+
 EXTRA_FLAGS=""
 BATCH_SIZE=32
 TP_SIZE=1
-while getopts "m:b:l:t:d:h:o:" OPT; do
+MULTI_NODE_SETUP=false
+while getopts "m:b:l:t:d:h:o:g:" OPT; do
     case ${OPT} in
         m )
             MODEL_PATH="$OPTARG"
@@ -81,6 +96,9 @@ while getopts "m:b:l:t:d:h:o:" OPT; do
             ;;
         t )
             TP_SIZE="$OPTARG"
+            ;;
+        g )
+            CARD_GROUPS="$OPTARG"
             ;;
         h )
             usage
@@ -102,7 +120,7 @@ fi
 MODEL_NAME=$(extract_last_folder_name "$MODEL_PATH")
 
 echo "Step 0 - detecting used device type [g2, g3]"
-DEVICE_TYPE=$(python step-0-detect-device.py) || (echo "Detecting device process failed" && exit 1)
+DEVICE_TYPE=$(python3 step-0-detect-device.py) || (echo "Detecting device process failed" && exit 1)
 DEVICE_TYPE="g$DEVICE_TYPE"
 echo "Detected device type: $DEVICE_TYPE"
 echo "Step 0 done"
@@ -111,6 +129,27 @@ echo "Step 0 done"
 if [[ ! " ${ALLOWED_DEVICES[*]} " =~ " $DEVICE_TYPE " ]]; then
     echo "Invalid device type: $DEVICE_TYPE. Allowed devices: ${ALLOWED_DEVICES[*]}"
     exit 1
+fi
+
+if [[ $TP_SIZE -gt 8 ]]; then
+    MULTI_NODE_SETUP=true
+fi
+
+if $MULTI_NODE_SETUP; then
+    RAY_AVAILABLE_RESOURCES=$(python3 -c 'import ray; ray.init(); print(int(ray.available_resources()["HPU"]))')
+    if [[ $RAY_AVAILABLE_RESOURCES -lt $TP_SIZE ]]; then
+        echo "Required TP size : $TP_SIZE" 
+        echo "Available HPU's : $RAY_AVAILABLE_RESOURCES "
+        echo "!! Exiting since not enough HPU resources available. You can run 'ray status' to see available resources"
+        echo "Refer https://github.com/HabanaAI/vllm-hpu-extension/tree/main/calibration#experimental-multi-node-fp8-calibration for multi-node runs"
+        exit 1
+    fi
+
+    if [[ ! -e $QUANT_CONFIG ]]; then
+        echo " !! Exiting. Invalid QUANT_CONFIG env"
+        echo " Multi-node calibration requires QUANT_CONFIG to point to an empty buffer.json file. Refer https://github.com/HabanaAI/vllm-hpu-extension/tree/main/calibration#experimental-multi-node-fp8-calibration"
+        exit 1
+    fi
 fi
 
 create_measure_config $FP8_DIR $MODEL_NAME $DEVICE_TYPE
@@ -130,25 +169,55 @@ if [[ -n $LIMIT ]]; then
     EXTRA_FLAGS+="--max-dataset-samples $LIMIT "
 fi
 
+if $MULTI_NODE_SETUP; then
+    cat $FP8_DIR/$MODEL_NAME/maxabs_measure_$DEVICE_TYPE.json > $QUANT_CONFIG
+    sleep 2
+else
+    export QUANT_CONFIG=$FP8_DIR/$MODEL_NAME/maxabs_measure_$DEVICE_TYPE.json
+fi
+
 echo ""
 echo "1/4 Preparing calibration dataset"
-export QUANT_CONFIG=$FP8_DIR/$MODEL_NAME/maxabs_measure_$DEVICE_TYPE.json
-python step-1-prepare-calibration-dataset.py -m $MODEL_PATH -d $DATASET_PATH -o $MODEL_NAME $EXTRA_FLAGS || (echo "Error in step 1" && exit 1)
+python3 step-1-prepare-calibration-dataset.py -m $MODEL_PATH -d $DATASET_PATH -o $MODEL_NAME $EXTRA_FLAGS || (echo "Error in step 1" && exit 1)
 echo "Step 1/4 done"
 
 echo ""
 echo "2/4 Measuring scales"
-python step-2-measure-scales.py -m $MODEL_PATH --tensor-parallel-size $TP_SIZE -d $MODEL_NAME-calibration-dataset.pkl --batch-size $BATCH_SIZE || (echo "Error in step 2" && exit 1)
+if $MULTI_NODE_SETUP; then
+    python3 step-2-measure-scales.py -m $MODEL_PATH --tensor-parallel-size $TP_SIZE -d $MODEL_NAME-calibration-dataset.pkl --batch-size $BATCH_SIZE --distributed-executor-backend ray || (echo "Error in step 2" && exit 1)
+else
+    python3 step-2-measure-scales.py -m $MODEL_PATH --tensor-parallel-size $TP_SIZE -d $MODEL_NAME-calibration-dataset.pkl --batch-size $BATCH_SIZE || (echo "Error in step 2" && exit 1)
+fi
 echo "Step 2/4 done"
 
 echo ""
 echo "3/4 Postprocessing scales"
-python step-3-postprocess_measure.py -m $FP8_DIR/$MODEL_NAME/$DEVICE_TYPE/ -o inc_tmp/$MODEL_NAME/$DEVICE_TYPE/ || (echo "Error in step 3" && exit 1)
+python3 step-3-postprocess_measure.py -m $FP8_DIR/$MODEL_NAME/$DEVICE_TYPE/ -o inc_tmp/$MODEL_NAME/$DEVICE_TYPE/ || (echo "Error in step 3" && exit 1)
 cp inc_tmp/$MODEL_NAME/$DEVICE_TYPE/* $FP8_DIR/$MODEL_NAME/$DEVICE_TYPE/
 echo "Step 3/4 done"
 
+
+if $MULTI_NODE_SETUP; then
+    cat $FP8_DIR/$MODEL_NAME/maxabs_quant_$DEVICE_TYPE.json > $QUANT_CONFIG
+    sleep 2
+else
+    export QUANT_CONFIG=$FP8_DIR/$MODEL_NAME/maxabs_quant_$DEVICE_TYPE.json
+fi
+
 echo ""
 echo "4/4 Quantize scales"
-export QUANT_CONFIG=$FP8_DIR/$MODEL_NAME/maxabs_quant_$DEVICE_TYPE.json
-python step-4-quantize-scales.py --model $MODEL_PATH --tensor-parallel-size $TP_SIZE || (echo "Error in step 4" && exit 1)
+if $MULTI_NODE_RUN; then
+    python3 step-4-quantize-scales.py --model $MODEL_PATH --tensor-parallel-size $TP_SIZE --distributed-executor-backend ray || (echo "Error in step 4" && exit 1)
+else
+    python3 step-4-quantize-scales.py --model $MODEL_PATH --tensor-parallel-size $TP_SIZE || (echo "Error in step 4" && exit 1)
+fi
+
+if [[ -n $CARD_GROUPS ]]; then
+    echo ""
+    echo "5/5 Unify scales"
+    QUANT_DIR=$FP8_DIR/$MODEL_NAME/$DEVICE_TYPE/
+    python3 step-5-unify_measurements.py -g "$CARD_GROUPS" -m $QUANT_DIR -o $QUANT_DIR || (echo "Error in step 5" && exit 1)
+    echo "Step 5/5 done"
+fi
+cleanup_tmp
 echo "Calibration process done"
