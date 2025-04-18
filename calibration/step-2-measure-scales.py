3e6657912ba573f393f3cc896e1195e151c77ba4
Krzysztof Wiśniewski <kwisniewski@habana.ai>
2025-04-18 14:14:57 +0200

    Add support of deepseek fp8 calibration (#155)
    * Fix calibration for deepseek
    
    * Remove debug log
    
    * Remove trust remote code parameter

--- calibration/step-2-measure-scales.py
+++ calibration/step-2-measure-scales.py
@@ -43,6 +43,7 @@ if __name__ == "__main__":
     parser.add_argument("--tensor-parallel-size", type=int, default=1)
     parser.add_argument("--max-dataset-samples", type=int, default=0)
     parser.add_argument("--max-num-prefill-seqs", type=int, default=1)
+    parser.add_argument("--block-quant", action="store_true", default=False)
     parser.add_argument("--max-model-len", type=int, default=2048)
     parser.add_argument("-v", "--verbose", action="store_true")
     parser.add_argument("--distributed-executor-backend", choices=["mp", "ray"], default="mp", 
@@ -51,11 +52,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     calibration_ds = get_ds(args)
-
     llm = vllm.LLM(
         model=args.model,
         dtype=torch.bfloat16,
-        quantization="inc",
+        quantization="fp8" if args.block_quant else "inc",
         max_num_seqs=args.batch_size,
         tensor_parallel_size=args.tensor_parallel_size,
         max_model_len=args.max_model_len,
