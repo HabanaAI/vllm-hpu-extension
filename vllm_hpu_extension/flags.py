2925fd5cb95cae065597a29ce3b4c5bbeb52cd59
Michal Adamczyk <madamczyk@habana.ai>
2025-04-28 14:08:48 +0300

    Reorder version checks and reorganize kernel specification

--- vllm_hpu_extension/flags.py
+++ vllm_hpu_extension/flags.py
@@ -162,8 +162,8 @@ def enabled_flags():
                            & Not(EnvFlag("VLLM_PROMPT_USE_FUSEDSDPA", "false"))
                            & EnvFlag("VLLM_PROMPT_USE_FLEX_ATTENTION", "false")),
         "fused_block_softmax_adjustment": (Not(Hardware("cpu"))
+                                           & VersionRange(">=1.22.0.101")
                                            & Kernel(block_softmax_adjustment)
-                                           & (VersionRange(">=1.22.0.101"))
                                            & EnvFlag("VLLM_FUSED_BLOCK_SOFTMAX_ADJUSTMENT",
                                                      Not(ModelType('qwen2')) & Hardware("gaudi3"))),
     }
