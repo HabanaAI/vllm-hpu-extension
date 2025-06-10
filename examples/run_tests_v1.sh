904da2da8efe6f5e7f88badf70707c59465832b7
Chendi.Xue <chendi.xue@intel.com>
2025-06-11 00:51:45 +0300

    quick fix torch.compile error when calling vocab_parallel_embedding
    original error was torch._inductor.exc.InductorError: KeyError 'hpu'
    TODO: figure out a way to not monkey patch
    
    Signed-off-by: Chendi.Xue <chendi.xue@intel.com>

--- examples/run_tests_v1.sh
+++ examples/run_tests_v1.sh
@@ -4,45 +4,45 @@
 
 # llama
 VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false \
-pytest -v -s ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Meta-Llama-3.1-8B-Instruct.yaml
+pytest -v ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Meta-Llama-3.1-8B-Instruct.yaml
 
 # APC tests
 VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false ENABLE_APC=1 \
-pytest -v -s ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Meta-Llama-3.1-8B-Instruct.yaml
+pytest -v ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Meta-Llama-3.1-8B-Instruct.yaml
 
 # Eager tests
 VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false ENFORCE_EAGER=1 \
-pytest -v -s ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Meta-Llama-3.1-8B-Instruct.yaml
+pytest -v ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Meta-Llama-3.1-8B-Instruct.yaml
 
 # hpu graph warmup 
 PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false \
-pytest -v -s ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Meta-Llama-3.1-8B-Instruct.yaml
+pytest -v ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Meta-Llama-3.1-8B-Instruct.yaml
 
 # TP=2
-VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false TP_SIZE=2 \
-pytest -v -s ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Meta-Llama-3.1-8B-Instruct.yaml
+PT_HPU_ENABLE_LAZY_COLLECTIVES=True VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false TP_SIZE=2 \
+pytest -v ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Meta-Llama-3.1-8B-Instruct.yaml
 
 # ======================== Model validate ========================== #
 # qwen3 moe
 VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false \
-pytest -v -s ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Qwen3-30B-A3B.yaml
+pytest -v ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Qwen3-30B-A3B.yaml
 
 # granite
 VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false \
-pytest -v -s ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/granite-8b.yaml
+pytest -v ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/granite-8b.yaml
 
 # mistral
 VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false \
-pytest -v -s ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Mistral-7B-Instruct-v0.3.yaml
+pytest -v ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Mistral-7B-Instruct-v0.3.yaml
 
 # mixtral TP=2
-VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false \
-pytest -v -s ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Mixtral-8x7B-Instruct-v0.1.yaml
+PT_HPU_ENABLE_LAZY_COLLECTIVES=True VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false \
+pytest -v ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Mixtral-8x7B-Instruct-v0.1.yaml
 
 # llama4-moe TP=4 (text generation)
-VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false \
-pytest -v -s ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Llama-4-Scout-17B-16E-Instruct.yaml
+PT_HPU_ENABLE_LAZY_COLLECTIVES=True VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false \
+pytest -v ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Llama-4-Scout-17B-16E-Instruct.yaml
 
-# llama4-moe TP=4 (vision ineference)
-VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false \
-pytest -v -s ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Llama-4-Scout-17B-16E-Instruct-vision.yaml
+# # llama4-moe TP=4 (vision ineference)
+# VLLM_SKIP_WARMUP=True PT_HPU_LAZY_MODE=1 VLLM_USE_V1=1 VLLM_CONTIGUOUS_PA=false \
+# pytest -v ../tests/models/language/generation/test_common.py --model_card_path ../tests/models/language/generation/model_cards/Llama-4-Scout-17B-16E-Instruct-vision.yaml
