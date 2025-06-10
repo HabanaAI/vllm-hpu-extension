dfb656e6957e0db90090e4d406935ee08d82f6b6
Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>
2025-06-10 16:23:15 +0300

    Move args linear
    Signed-off-by: Agata Dobrzyniewicz <adobrzyniewicz@habana.ai>

--- vllm_hpu_extension/bucketing/linear.py
+++ vllm_hpu_extension/bucketing/linear.py
@@ -24,8 +24,8 @@ class HPUBucketingContext(metaclass=WeakSingleton):
     global_state = HPUBucketingGlobalState()
 
     def __init__(self, max_num_seqs, max_num_prefill_seqs, block_size,
-                 max_num_batched_tokens, use_merged_prefill, max_model_len,
-                 max_prompt_seq=None, max_decode_seq=None, prefix_caching=True):
+                 max_num_batched_tokens, use_merged_prefill, prefix_caching, 
+                 max_model_len, max_prompt_seq=None, max_decode_seq=None):
         """
         Initializes the bucketing parameters for sequence padding.
 
@@ -110,8 +110,8 @@ class HPUBucketingContext(metaclass=WeakSingleton):
             self.global_state.prompt_bs_bucket_cfg,
             self.global_state.prompt_seq_bucket_cfg,
             self.block_size,
-            self.max_num_batched_tokens,
-            self.prefix_caching)
+            self.prefix_caching,
+            self.max_num_batched_tokens)
 
         msg = (f"Generated {len(self.global_state.prompt_buckets)} "
                f"prompt buckets [bs, seq]: "
@@ -236,8 +236,8 @@ def warmup_range(config: Tuple[int, int, int]):
 def generate_prompt_buckets(bs_bucket_config,
                             seq_bucket_config,
                             block_size,
-                            max_num_batched_tokens=None,
-                            prefix_caching=True):
+                            prefix_caching,
+                            max_num_batched_tokens=None):
     _, _, bmax = seq_bucket_config
     batch_size_buckets = warmup_range(bs_bucket_config)
     seq_bucket_config = warmup_range(seq_bucket_config)
