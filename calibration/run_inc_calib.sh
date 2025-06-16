e4f6d5a9c92606b6f627af48c2a2010b1233c8a4
yiliu30 <yi4.liu@intel.com>
2025-06-16 06:13:19 +0000

    auto prepare dataset support
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

---
+++ calibration/run_inc_calib.sh
@@ -0,0 +1,43 @@
+DEFAULT_MODEL_PATH="/mnt/disk3/DeepSeek-R1-G2-INC"
+FP8_MODEL_PATH="${1:-$DEFAULT_MODEL_PATH}"
+
+QUANT_CONFIG_FILE="scripts/quant_configs/inc_measure_config.json"
+timestamp=$(date +%Y%m%d_%H%M%S)
+LOG_FILE="prepare.pile.512.${timestamp}.log"
+
+# remove nc_workspace_measure if needed
+if [ -e nc_workspace_measure ]; then
+    echo "The directory nc_workspace_measure already exists, removing it..."
+    rm -rf nc_workspace_measure
+fi
+
+
+echo "============ QUANT_CONFIG file content ==============="
+cat ${QUANT_CONFIG_FILE}
+echo "======================================================"
+
+
+
+echo "Start INC calibration with model ${FP8_MODEL_PATH}, log file ${LOG_FILE}"
+
+
+VLLM_HPU_FORCE_CHANNEL_FP8=0 \
+PT_HPU_LAZY_MODE=1 \
+VLLM_MLA_PERFORM_MATRIX_ABSORPTION=0 \
+VLLM_ENABLE_RUNTIME_DEQUANT=1 \
+VLLM_REQUANT_FP8_INC=1 \
+VLLM_MOE_N_SLICE=1 \
+QUANT_CONFIG=${QUANT_CONFIG_FILE} \
+    python step-2-measure-scales.py \
+    --model ${FP8_MODEL_PATH} \
+    --tokenizer ${FP8_MODEL_PATH} \
+    --max-tokens 32 \
+    --batch-size 1 \
+    --max-dataset-samples 512 \
+    --auto-process-dataset \
+    --sample-len 1024 \
+    --max-model-len 2048 \
+    --verbose \
+    --tensor-parallel-size 8 \
+    --expert-parallel \
+    --dataset pile 2>&1 | tee $LOG_FILE
