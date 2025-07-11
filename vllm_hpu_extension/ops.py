eeb2977235338410be49f09adc2ea9a5f7c27035
Wang, Mengni <mengni.wang@intel.com>
2025-07-11 09:27:12 +0800

    Update ops.py

--- vllm_hpu_extension/ops.py
+++ vllm_hpu_extension/ops.py
@@ -167,6 +167,7 @@ def flat_pa(query, key_cache, value_cache, block_list, block_mapping,
     attn = matmul_qk_op(query, key)
     if 'fp32_softmax' in enabled_flags():
         attn = attn.float()
+        htcore.mark_step()
     attn = attn + block_bias
     attn = pipelined_pa(attn, value, block_groups, block_mapping,
                         batch_size=batch_size, matmul_av_op=matmul_av_op,
