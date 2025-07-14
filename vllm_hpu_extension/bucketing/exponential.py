790b14790d957e6150e493f2e19f602f0c40da19
Iryna Boiko <iboiko@habana.ai>
2025-07-14 13:54:40 +0300

    Fix for warmup third dim

--- vllm_hpu_extension/bucketing/exponential.py
+++ vllm_hpu_extension/bucketing/exponential.py
@@ -103,8 +103,8 @@ def generate_prompt_buckets(bs_bucket_config,
         for bs in batch_size_buckets:
             for b in seq_bucket_config:
                 max_blocks_range = (bmax - b) // block_size
-                if max_blocks_range == 0:
-                    buckets_3d.append((bs, b, 0))
+                if max_blocks_range == 0 or max_blocks_range == 1:
+                    buckets_3d.append((bs, b, max_blocks_range))
                     continue
                 else:
                     num_buckets_3d = math.ceil(math.log2(max_blocks_range)) + 1
