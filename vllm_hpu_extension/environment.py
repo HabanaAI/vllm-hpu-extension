e87950c32246eecf4ffe28bad44eec2c87d2672a
Michal Adamczyk <madamczyk@habana.ai>
2025-07-16 12:55:31 +0200

    Validate config values upon reading (#274)

--- vllm_hpu_extension/environment.py
+++ vllm_hpu_extension/environment.py
@@ -6,7 +6,8 @@
 ###############################################################################
 
 from .logger import logger
-from .config import Value, choice, boolean, split_values_and_flags
+from .config import Value, boolean, split_values_and_flags
+from .validation import choice, regex
 
 
 _VLLM_VALUES = {}
@@ -83,10 +84,10 @@ def VllmValue(name, env_var_type):
 
 def get_environment():
     values = [
-        Value('hw', _get_hw, env_var_type=choice('cpu', 'gaudi', 'gaudi2', 'gaudi3')),
-        Value('build', _get_build, env_var_type=str),
+        Value('hw', _get_hw, env_var_type=str, check=choice('cpu', 'gaudi', 'gaudi2', 'gaudi3')),
+        Value('build', _get_build, env_var_type=str, check=regex(r'^\d+\.\d+\.\d+\.\d+$', hint='You can override detected build by specifying VLLM_BUILD env variable')),
         Value('engine_version', _get_vllm_engine_version, env_var_type=str),
-        Value('bridge_mode', _get_pt_bridge_mode, env_var_type=choice('eager', 'lazy')),
+        Value('bridge_mode', _get_pt_bridge_mode, env_var_type=str, check=choice('eager', 'lazy')),
         VllmValue('model_type', str),
         VllmValue('prefix_caching', boolean),
     ]
---
+++ vllm_hpu_extension/validation.py
@@ -0,0 +1,32 @@
+###############################################################################
+# Copyright (C) 2025 Intel Corporation
+#
+# This source code is licensed under the Apache 2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+###############################################################################
+
+from typing import Callable, Any, Optional, TypeAlias
+import re
+
+Error: TypeAlias = str
+Checker: TypeAlias = Callable[[Any], Optional[Error]]
+
+def choice(*options: Any) -> Checker:
+    """Validates if input is one of the available choices"""
+    def choice_impl(x):
+        if x not in options:
+            return f'{x} is not in allowed options: [{", ".join(map(str, options))}]!'
+    return choice_impl
+
+
+def regex(pattern, hint='') -> Checker:
+    """Validates if input matches pattern, optionally providing a hint in case of error"""
+    def regex_impl(value: str):
+        if not re.match(pattern, value):
+            return f"'{value}' doesn't match pattern '{pattern}'! {hint}"
+    return regex_impl
+
+
+def skip_validation(_) -> None:
+    """Dummy Checker used to skip validation"""
+    return None
