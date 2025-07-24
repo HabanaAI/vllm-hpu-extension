009adb2a42f0c22cc2add6347d17e460f3f50d9b
Mohit Deopujari <mdeopujari@habana.ai>
2025-07-24 04:29:31 -0700

    Cherry-pick window FusedSDPA for Gemma3  onto v1.22 (#302)
    * Rebase PR299 onto v1.22
    
    * Revert "Rebase PR299 onto v1.22"
    
    This reverts commit f447072626822f2261bb4503cbe51580e80ca361.
    
    * Add support for FusedSDPA with window_size (#285)
    
    * Fix error for PatchedModuleFusedSDPA (#296)
    
    * Fix error for PatchedModuleFusedSDPA
    
    * Revert "Fix ModuleFusedSDPA graph break (#257)"
    
    This reverts commit dc0a2b4cc9d4866a25457c0f8f360412926b4cea.
    
    * Fix args updates for window FusedSDPA correctly (#299)
    
    ---------
    
    Co-authored-by: Jimin Ha <jimin.ha@intel.com>

--- vllm_hpu_extension/ops.py
+++ vllm_hpu_extension/ops.py
@@ -300,11 +300,13 @@ def _fsdpa_prompt_attention(
         is_causal: bool,
         attn_bias: Optional[torch.Tensor] = None,
         valid_seq_lengths: Optional[torch.Tensor] = None,
+        window_size: Optional[int] = None,
         **ignored_args
 ) -> torch.Tensor:
     query = query.transpose(1, 2)
     key = key.transpose(1, 2)
     value = value.transpose(1, 2)
+    padding_side = 'right'
     if get_config().fp32_softmax:
         softmax_mode = 'fp32'
     else:
@@ -316,9 +318,20 @@ def _fsdpa_prompt_attention(
         # TODO: causal + attn_bias is not yet supported
         is_causal = False
         valid_seq_lengths = None
-    attn_weights = fsdpa_op(query, key, value, attn_bias, 0.0, is_causal,
-                            scale, softmax_mode, recompute_mode,
-                            valid_seq_lengths, 'right')
+
+    if window_size is not None:
+        #causal window sdpa kernel only supports softmax None
+        softmax_mode = 'None'
+        padding_side ='left'
+
+    args = [query, key, value, attn_bias, 0.0, is_causal,
+                                scale, softmax_mode, recompute_mode,
+                                valid_seq_lengths, padding_side]
+    args += [window_size] if window_size else []
+
+
+    attn_weights = fsdpa_op(*args)
+
     attn_weights = attn_weights.transpose(1, 2)
     return attn_weights
 
