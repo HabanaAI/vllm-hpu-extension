dcc549a0fdf602330740ebe8cb911c89365bea64
tianyuan211 <paulyuen@umich.edu>
2025-08-22 16:58:42 +0800

    Update ops.py

--- vllm_hpu_extension/ops.py
+++ vllm_hpu_extension/ops.py
@@ -1044,7 +1044,12 @@ class VllmMixtureOfExpertsOpFP8PerChannel(torch.nn.Module):
                                     **kwargs)
         else:
             x_scale = self.w13_input_scale.data
-            w2_input_scale = self.w2_input_scale
+            
+            if get_config().model_type == "hunyuan":
+                w2_input_scale = self.w2_input_scale
+            else:
+                w2_input_scale = self.w2_input_scale.data
+                
             x_fp8 = torch.ops.hpu.cast_to_fp8_v2(x, 1.0/x_scale, False, False, torch.float8_e4m3fn)[0]
             final_hidden_states = torch.ops.hpu.mixture_of_experts(
                                     hidden_states=x_fp8,
