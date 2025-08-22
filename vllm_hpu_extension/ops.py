1d2a4b22911aaf9bc01287bf0b054f7bfd8e76b9
tianyuan211 <paulyuen@umich.edu>
2025-08-22 15:01:42 +0800

    Update ops.py

--- vllm_hpu_extension/ops.py
+++ vllm_hpu_extension/ops.py
@@ -849,7 +849,7 @@ def fp8_channel_moe_prepare_weights(layer):
     htorch.core.mark_step()
     
     
-    print('\n\n\n',layer.moe_op,'\n\n\n')
+    print('\n\n\n', layer.moe_op.w13_list[0].weight.shape, layer.moe_op.w2_list[0].weight.shape, '\n\n\n')
     
     
     return layer
