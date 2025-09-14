b47281ffa27191edaefb3203142bfe54c56a7dd9
KepingYan <keping.yan@intel.com>
2025-09-14 14:59:55 +0800

    Enable chunked prefill

--- vllm_hpu_extension/cache_ops.py
+++ vllm_hpu_extension/cache_ops.py
@@ -31,6 +31,27 @@ def swap_blocks_hpu_cpu(src: torch.Tensor, dst: torch.Tensor, block_mapping_t: t
 
     dst.index_copy_(0, dst_indices, src.index_select(0, src_indices))
 
+def insert_or_update_cache_chunked(input, cache, block_indices, block_offsets):
+    if block_offsets is None:
+        cache.index_copy_(0, block_indices, input)
+    else:
+        if block_offsets.numel() == block_indices.numel():
+            cache.index_put_((block_indices, block_offsets), input)
+        else:
+            offsets = None
+            block_size = cache.shape[1]
+            for i in range(block_indices.shape[0]):
+                offsets = block_offsets[i * block_size:(i + 1) * block_size - 1]
+                offset_indices = (offsets == -1)
+                offset_indices = offset_indices.nonzero(as_tuple=True)
+                start_index = offsets[0].item()
+                if offset_indices[0].numel() == 0:
+                    temp_index = offsets[offsets.numel() - 1].item()
+                else:
+                    temp_index = offset_indices[0][0].item()
+                end_index = offsets[temp_index - 1].item() + 1
+                cache[block_indices[i], start_index:end_index] = input[i][:temp_index]
+
 def swap_blocks(src, dst, block_mapping):
     if block_mapping.numel() == 0 or dst is None or src is None:
         return
