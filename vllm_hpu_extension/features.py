b76a47d2001804abdd6c9b70e974493567616764
Kurt Chen <kurtchen@habana.ai>
2026-01-08 15:56:57 +0800

    Add FusedSDPA QKV slice support for APC (#393)
    * Add FusedSDPA QKV slice support for APC
    
    The function can be controlled by following env:
      VLLM_FUSEDSDPA_QKV_SLICE_SEQ_LEN_THLD
        When query or context exceeds this threshold, enable QKV slice.
        Default threshold is 8192.
        Set to 0 to disable this function.
      VLLM_FUSEDSDPA_Q_SLICE_CHUNK_SIZE
        The Q chunk size for full attention part.
      VLLM_FUSEDSDPA_KV_SLICE_CHUNK_SIZE
        The KV chunk size for full attention part.
      VLLM_FUSEDSDPA_CAUSAL_QKV_SLICE_CHUNK_SIZE
        The QKV chunk size for causal attention part.
    
    * Update APC FusedSDPA slice to align with FP8 impl in INC.
    
    VLLM_HPU_FSDPA_SLICE_SEQ_LEN_THLD (int):
        QKV slice threshold, default 8192, set 0 to disable.
    
    VLLM_HPU_FSDPA_SLICE_CHUNK_SIZE (int):
        QKV slice size, default 4096.
    
    VLLM_HPU_FSDPA_SLICE_IMPL (str):
        slice_qkv (default): split prefix & causal, and do QKV slice on both parts.
        slice_causal: split prefix & causal, and do QKV slice on causal part only.
        split_kv: split prefix & causal, no QKV slice.
    
    VLLM_HPU_FSDPA_SLICE_CAUSAL (boolean):
        Also do QKV slice in non APC case when query len greater than threshold.
    
    * Disable FusedSDPA QKV split by default
    
    * fix causal mask for non-gqa case

--- vllm_hpu_extension/features.py
+++ vllm_hpu_extension/features.py
@@ -34,14 +34,17 @@ def get_user_flags():
         # Non-vllm flags that are also important to print
         Env('EXPERIMENTAL_WEIGHT_SHARING', str),
         Env('PT_HPU_WEIGHT_SHARING', str),
-        
+
         # Sliding window flags
         Env('PT_HPU_SDPA_QKV_SLICE_MODE_FWD', boolean),
         Env('PT_HPU_SDPA_BC_FACTOR', int),
         Env('VLLM_FUSEDSDPA_SLIDE_THLD', int),
+
+        # FusedSDPA slice flags
         Env('VLLM_HPU_FSDPA_SLICE_SEQ_LEN_THLD', int),
         Env('VLLM_HPU_FSDPA_SLICE_CHUNK_SIZE', int),
         Env('VLLM_HPU_FSDPA_SLICE_IMPL', str),
+        Env('VLLM_HPU_FSDPA_SLICE_CAUSAL', boolean),
     ]
     return to_dict(flags)
 
